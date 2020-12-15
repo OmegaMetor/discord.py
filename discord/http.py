6b22b9c34917c73e4bff0b022906b50a605225f0
OmegaMetor <42892531+OmegaMetor@users.noreply.github.com>
2020-12-15 09:08:52 -0600

    Update http.py
    fixed other syntax error

--- discord/http.py
+++ discord/http.py
@@ -110,7 +110,6 @@ class HTTPClient:
         self.use_clock = not unsync_clock
 
         user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
-'
         self.user_agent = user_agent
 
     def recreate(self):
