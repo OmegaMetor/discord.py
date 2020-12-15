c769016bff74ab442956c5059a027457a03107f8
OmegaMetor <42892531+OmegaMetor@users.noreply.github.com>
2020-12-15 09:02:59 -0600

    Update http.py

--- discord/http.py
+++ discord/http.py
@@ -109,8 +109,9 @@ class HTTPClient:
         self.proxy_auth = proxy_auth
         self.use_clock = not unsync_clock
 
-        user_agent = 'DiscordBot (https://github.com/Rapptz/discord.py {0}) Python/{1[0]}.{1[1]} aiohttp/{2}'
-        self.user_agent = user_agent.format(__version__, sys.version_info, aiohttp.__version__)
+        user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36
+'
+        self.user_agent = user_agent
 
     def recreate(self):
         if self.__session.closed:
